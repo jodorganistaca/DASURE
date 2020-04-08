fc0efa7c83aac729e1400a053c364aeb196fd36c
Juan Sebastián Bravo <js.bravo@uniandes.edu.co>
2020-04-08 09:31:53 -0500

    Fixed linting in authentication files

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -10,69 +10,69 @@ const jwt = require("jsonwebtoken");
 router.get("/google", passport.authenticate("google", { scope: ["email", "profile"] }));
 
 router.post("/", function(req, res) {
-  const { email, password } = req.body;
-  res.clearCookie('x-access-token');
-  console.log(email);
-            db.findOneObjectPromise("application", "users", {email: email})
-            .then(user => {
-              if(!user&&user[0])
-              {
+    const { email, password } = req.body;
+    res.clearCookie("x-access-token");
+    console.log(email);
+    db.findOneObjectPromise("application", "users", {email: email})
+        .then(user => {
+            if(!user&&user[0])
+            {
                 return res
                     .status(400)
                     .json({ errors: [{ msg: "Invalid Credentials" }] });
-              }
-              else
-              {
+            }
+            else
+            {
                 console.log("user", user);
                 const usr = user[0];
                 console.log("usr", usr);
                 bcrypt.compare(password, usr.password)
-                .then(match => {
-                  if(!match)
-                  {
-                    return res
-                    .status(400)
-                    .json({ errors: [{ msg: "Invalid Credentials" }] });
-                  }
-                  else
-                  {
-                    const payload = {
-                      user: {
-                          _id: usr._id,
-                          email: usr.email,
-                          name: usr.name
-                      },
-                  };
-                  console.log("create", process.env.JWT_SECRET);
-                  jwt.sign(
-                      payload,
-                      process.env.JWT_SECRET,
-                      { expiresIn: 36000 },
-                      (error, token) => {
-                          if (error) throw error;
-                          let options = {
-                            path:"/",
-                            sameSite:true,
-                            maxAge: 1000 * 60 * 60 * 24, // would expire after 24 hours
-                            httpOnly: true, // The cookie only accessible by the web server
+                    .then(match => {
+                        if(!match)
+                        {
+                            return res
+                                .status(400)
+                                .json({ errors: [{ msg: "Invalid Credentials" }] });
                         }
-                    
-                        res.cookie('x-access-token',token, options);
-                        res.redirect("../")
-                      });
-                  }
-                })
-              }
-            })
+                        else
+                        {
+                            const payload = {
+                                user: {
+                                    _id: usr._id,
+                                    email: usr.email,
+                                    name: usr.name
+                                },
+                            };
+                            console.log("create", process.env.JWT_SECRET);
+                            jwt.sign(
+                                payload,
+                                process.env.JWT_SECRET,
+                                { expiresIn: 36000 },
+                                (error, token) => {
+                                    if (error) throw error;
+                                    let options = {
+                                        path:"/",
+                                        sameSite:true,
+                                        maxAge: 1000 * 60 * 60 * 24, // would expire after 24 hours
+                                        httpOnly: true, // The cookie only accessible by the web server
+                                    };
+                                    console.log("Token created", token);
+                                    res.cookie("x-access-token",token, options);
+                                    res.redirect("../");
+                                });
+                        }
+                    });
+            }
+        });
 });
 
 router.get(
-  "/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-     // Successful authentication, redirect home.
-     res.redirect("../");
-  }
+    "/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("../");
+    }
 );
 
 
